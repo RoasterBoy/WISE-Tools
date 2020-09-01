5ff253207fb6999dda64fa7c9f442bcfd56f8158
Karl A. Hakkarainen <kh@queenlake.com>
2020-09-01 09:17:35 -0400

    Add files via upload
    Starting cleanup and standardization (help, msg, setup functions, etc.)

--- get.events.sh
+++ get.events.sh
@@ -12,7 +12,20 @@
 # API key must be encoded to base64 with the prefix APIKEY:
 # For example, APIKEY:WFVHjqxMsz9k637XCI64bEahTmO7gjv
 #
-tmp=tmp
+### get.events - get Wild Apricot events
+###
+### Usage:
+###   get.events.sh [options]
+###
+### Options:
+help()
+{
+    thisScript=$(which "$0")
+    sed -rn 's/^\s*### ?//;T;p' "$thisScript"
+    exit
+    }
+tmp=/tmp/wa
+outdir=/tmp/out
 getReg=false
 eventFilter=""
 tagFilter=""
@@ -24,36 +37,33 @@ setup ()
 #
     mkdir -p $tmp
     touch $tmp/tmp.tmp
-    rm $tmp/*
-#
-} # end setup
+    rm -r $tmp/*
+    mkdir -p $outdir
+    touch $outdir/tmp.tmp
+    rm $outdir/*
+##
 # 
+# end setup
+}
 getAuth()
 {
-thisKey=`echo -ne "APIKEY:$WA_key"|base64`
-curl -s  --header "Content-Type:application/x-www-form-urlencoded" \
-     --header "Authorization: Basic $thisKey" -d "grant_type=client_credentials&scope=auto"\
-     https://oauth.wildapricot.org/auth/token  -o $tmp/token.json
-# 
-thisAuth=$(jq -r '.access_token' $tmp/token.json)
-#
+    thisAuth=$(get.auth.sh)
 }
 getEvents ()
 {
     case $# in
 	 0) filterString=""
 	    ;;
-	 1) filterString="?$filter=$1"
+	 1) filterString="?\$filter=$1"
 	    ;;
-	 2) filterString="?filter=$1 AND $2"
+	 2) filterString="?\$filter=$1 AND $2"
 	    ;;
 	 *)
 	     echo "Bad number of parameters"
     esac
-    
 curl -s --header "Authorization: Bearer $thisAuth"\
-     'https://api.wildapricot.org/v2.1/accounts/'"$WA_account"'/Events'$filterString -o $tmp/events.json
-jq '.[] | sort_by(.Name) | .[].Id | rtrimstr(",")' $tmp/events.json > $tmp/events.list
+     'https://api.wildapricot.org/v2.2/accounts/'"$WA_account"'/Events'"$filterString" -o $tmp/events.json
+jq '.[] | sort_by(.Id) | .[].Id | rtrimstr(",")' $tmp/events.json > $tmp/events.list
 }
 
 #--------------------------------------------------------- 
@@ -67,47 +77,71 @@ while IFS='' read -r thisEvent || [[ -n "$thisEvent" ]]; do
     if  $getReg ; then
 	if [ ! -f $tmp/$thisEvent-registrations.json ]; then
 	    curl -s --header "Authorization: Bearer $thisAuth"\
- 		 "https://api.wildapricot.org/v2.1/accounts/$WA_account/eventregistrations?eventId=$thisEvent&includeWaitList=true" \
+ 		 "https://api.wildapricot.org/v2.2/accounts/$WA_account/eventregistrations?eventId=$thisEvent&includeWaitList=true" \
 		 -o $tmp/$thisEvent-registrations.json
-	    fi
+	fi
+#	jq --raw-output '.[] | [.Event.Id,.Contact.Id, .OnWaitlist] | @tsv' $tmp/$thisEvent-registrations.json > $outdir/$thisEvent-registration.tsv
     fi
     if [ ! -f $tmp/$thisEvent-details.json ]; then
 	curl -s --header "Authorization: Bearer $thisAuth"  \
-	     "https://api.wildapricot.org/v2.1/accounts/$WA_account/events/$thisEvent"\
+	     "https://api.wildapricot.org/v2.2/accounts/$WA_account/events/$thisEvent"\
 	     -o $tmp/$thisEvent-details.json
+	# TODO Not sure why this file might not exist
+	# TODO If we stored this stuff in a database, we could just update as needed.
+	if [ -e $tmp/$thisEvent-details.json ]
+	then
+	    thisName=$(jq '.Name' $tmp/$thisEvent-details.json)
+	fi
+	msg "Working on $thisName"
 	fi
 done  < $tmp/events.list
 }
-getHelp() {
-    printf "%s\n" "Usage:"
-    printf "%s\n" "-u Get upcoming events"
-    printf "%s\n" "-t Use these tags. Tags must be comma-separated in a quoted string: \"fall,spring\""
-    printf "%s\n" "-r Get registration. Default is 'no'."
-    printf "%s\n" "-h Prints this message"
-    exit
-    }
-while getopts "ue:t:hr" opt; do
+getSingle()
+{
+curl -s --header "Authorization: Bearer $thisAuth"\
+     'https://api.wildapricot.org/v2.2/accounts/'"$WA_account"'/Events/'$eventId -o $tmp/$eventId.json
+}
+
+msg()
+{
+    if [  "$verbose" = true ];
+    then
+#       echo "Verbose is $verbose"                                                                                                  
+        thisString=$@
+        echo "%% $thisString"
+    fi
+}
+while getopts "ue:s:t:hvr" opt; do
     case ${opt} in
 	e ) #get this one event
-	    thisEvent=$OPTARG
-	    eventFilter='/$thisEvent'
+	    ### -e Get single event eventId
+	    eventId=$OPTARG
 	    ;;
 	u ) # Get Upcoming Events
+	    ### -u Get upcoming events
 	    echo "Getting upcoming events"
 	    eventFilter='IsUpcoming%20eq%20true'
 	    ;;
+
 	r ) # Do we need registrations
+	    ### -r Get registration. Default is 'no'.
 	    getReg=true
 	    ;;
 	t ) # Tags
+	    ### -t Use these tags. Tags must be comma-separated in a quoted string: \"fall,spring\"
 	    tags=$OPTARG
 	    tagFilter="Tags%20in%20%5B$tags%5D"
 	    ;;
+	v ) #Verbose
+	    ### -v Verbose mode
+	    verbose=true
+	    ;;
 	h) # Print help and exit
-	    getHelp
+	    ### -h Prints this message
+	    help
 	    ;;
 	?)
-	    getHelp
+	    help
 	    ;;
 	
     esac
@@ -117,8 +151,13 @@ done
 # 
 setup
 getAuth
-echo "Getting events, putting details in $tmp"
-getEvents $tagFilter $eventFilter
-if [ $getReg = "true" ]; then
-   mungFiles
-   fi 
+msg "Getting events, putting details in $tmp"
+if [ -z ${eventId} ]; then
+    getEvents $tagFilter $eventFilter
+    if [ $getReg = "true" ]; then
+	msg "This may take a long time."
+	mungFiles
+    fi 
+else
+    getSingle $eventId
+fi
