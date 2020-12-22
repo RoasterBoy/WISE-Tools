29c90d503e1d85a6d74918ea2f1fcc21696402c7
Karl Hakkarainen <kh@queenlake.com>
2020-12-22 17:06:33 -0500

    Getting closer

--- zoom.create.sh
+++ zoom.create.sh
@@ -35,12 +35,6 @@ init()
  }
  createMtg()
  {
-     # Make sure that we have all of the values needed
-     if [[ -z $DB || -z $HOST || -z $DATE ]]; then
-	 echo 'one or more variables are undefined'
-	 exit 1
-	 fi
- }
  ### Creates a Zoom meeting
  ### Required parameters
  ### 1 - Topic. Must be a quoted string
@@ -48,14 +42,14 @@ init()
  ### 3 - Number of class meetings, typically 5
  ### 4 - End date-time.  Must be a valid date-time string "YYY-MM-DD HH:MMp"
 
- topic=$1
- repeats=$3
- password=$(date '+%s')
- dow=$(date --date="$2" +%u)
- thisDow=$((dow + 1)) 
- thisStart=$(date --date="$2" +"%Y-%m-%dT%H:%M:00")
- thisEnd=$(date --date="$4" +"%Y-%m-%dT%H:%M:00")
-cat <<Endofmessage >payload.json
+     # Make sure that we have all of the values needed
+     if [[ -z $topic || -z $repeats || -z $thisStart ]]; then
+	 echo 'one or more variables are undefined'
+	 exit 1
+	 fi
+
+
+ cat <<Endofmessage >payload.json
 {
     "host_id": "sZbpHXjlQxiQHTLao_-paA",
     "topic": "$topic",
@@ -65,10 +59,11 @@ cat <<Endofmessage >payload.json
     "password": "$password",
     "recurrence": {
 	"type": 2,
-	"weekly_days": $thisDow,
-	"Repeat_interval": $repeats,
+	"weekly_days": "$thisDow",
+	"repeat_interval": $repeats,
 	"end_date_time": "$thisEnd"
     },
+    "start_time": "$thisStart",
     "settings": {
 	"join_before_host": true,
 	"jbh_time": 15,
@@ -78,20 +73,33 @@ cat <<Endofmessage >payload.json
 	"global_dial_in_countries": [
 	    "US"
 	]},
-
-
-  	    "tracking_fields": [
-	    		{
-				    "field": "Meeting type",
-				     "value": "Course",
-		    "visible": true
-          }]
-    }
+	"tracking_fields": [
+			   {
+			    "field": "Meeting type",
+			     "value": "Course",
+			     "visible": true
+			   }
+			   ]
+}
 
 
 Endofmessage
 #
+ }
+ topic="$1"
+ repeats="$3"
+  password=$(date '+%s')
+ dow=$(date --date="$2" +%u)
+ thisDow=$((dow + 1)) 
+ thisStart=$(date --date="$2" +"%Y-%m-%dT%H:%M:00")
+ thisEnd=$(date --date="$4" +"%Y-%m-%dT%H:%M:00")
+ echo $topic
+ echo $repeats
+ echo $thisStart
+ 
 
+ createMtg
+ 
 #python -mjson.tool payload.json
 #exit
 # Now do it
