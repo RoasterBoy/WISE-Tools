804c69105f7cf1f99a1df3dd5518a7eca34bbaa1
Karl Hakkarainen <kh@queenlake.com>
2020-12-29 16:58:07 -0500

    Think that it's working

--- update.wp.page.sh
+++ update.wp.page.sh
@@ -9,12 +9,19 @@
 #
 # mactarnahans.dreamhost.com
 #
-# Next step is to run it from my local system via ssh
+# 12/29 1PM
+# with ~/.wp-cli/config.yml in place
+# I can use
+# wp  --path=/home/karlh9n/wiselearners.org
+# to execute remote commands 
 #
+# In order to get the content over there, it needs to be piped in. (There may be a way to do this better,
+# but this works.
+# 
 # post number is $1
 # file is $2
 # title is $3
 postNumber=$1
 postFile=$2
 #wp --ssh karlh9n@mactarnahans.dreamhost.com update 944 --post_title="New Stuff" --debug course.list.html
-wp post update $postNumber  $postFile --ssh karlh9n@mactarnahans.dreamhost.com:~/wiselearners.org
+cat   $postFile  | wp --debug --path=/home/karlh9n/wiselearners.org  post update $postNumber -
