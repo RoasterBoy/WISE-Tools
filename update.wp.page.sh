f9b8a11b97fd71eb1207c7158b54c019076a1f21
Karl Hakkarainen <kh@queenlake.com>
2020-12-31 16:59:56 -0500

    Still munging

--- update.wp.page.sh
+++ update.wp.page.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+msg()
+{
+    if [  "$verbose" = true ];
+    then
+	msgString=$*
+	echo "%% $msgString"
+    fi
+}
+die() {
+    verbose=true
+    local msg=$1
+    local code=${2-1} # default exit status 1
+    msg "Error: $msg %%"
+    exit "$code"
+}
+
 # This is still a work in progress.
 #
 # Use wp-cli to replace the content of a page
@@ -24,4 +40,47 @@
 postNumber=$1
 postFile=$2
 #wp --ssh karlh9n@mactarnahans.dreamhost.com update 944 --post_title="New Stuff" --debug course.list.html
-cat   $postFile  | wp --debug --path=/home/karlh9n/wiselearners.org  post update $postNumber -
+
+updatePost()
+{
+    postNumber=$1
+    postFile=$2
+    if [ ! -f "$postFile" ]; then
+	die "Missing content file $postFile"
+    fi
+#    cat   $postFile  | wp --debug --path=/home/karlh9n/wiselearners.org  post update $postNumber -
+}
+getPosts()
+{
+    msg "Getting posts"
+    wp  --path=/home/karlh9n/wiselearners.org --stdout export > everything.xml
+    }
+
+help()
+{
+    echo "No help yet"
+}
+while getopts "gu:hv" opt; do
+    shopt -s nocasematch
+    case ${opt} in
+	    u )
+	    updatePost $1 $2
+	    ;;
+	g )
+	    getPosts
+	    ;;
+	h )
+	    help
+	    ;;
+	v )
+	    verbose="true"
+	    ;;
+	\?)
+	    help
+	    ;;
+        * )
+	    help
+	    ;;
+    esac
+done
+
