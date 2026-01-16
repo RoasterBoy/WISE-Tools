ab75c049a6decf059ebd8f658da6cbe7c89ed69e
RoasterBoy <kh@queenlake.com>
2026-01-16 16:15:06 -0500

    Fixed #! to run bash

--- getAISummary.sh
+++ getAISummary.sh
@@ -1,19 +1,15 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if [ -z "$1" ]; then
     echo "Missing meeting ID.."
     exit
 fi
 
-getHosts()
-{
-    hostList=hosts.list
-    :>$hostList
-    echo "Getting hosts"
-    curl -s --location --request GET 'https://api.zoom.us/v2/users?status=active&page_size=30' \
-	 --header 'Authorization: Bearer '"$thisAuth" -o hosts.json
-    jq -r '.users[] | select(.type == 2)  | .id' hosts.json > $hostList
-    echo "Got hosts"
-    exit
-    }
+echo "Client: $Zoom_OAuth_CLIENT"
+echo "Secret: ${Zoom_OAuth_SECRET:0:5}..."  # Only show first 5 chars for security
+echo "Account: $Zoom_OAuth_ACCOUNT"
+
+
 dblEncode()
 {
 #    echo $1
@@ -25,8 +21,9 @@ dblEncode()
 thisMeeting=$(echo "$1" | tr -d [:blank:])
 echo $thisMeeting
 credentials=$(echo -n $Zoom_OAuth_CLIENT:$Zoom_OAuth_SECRET | base64)
-thisAuth=$(curl -s --location 		--request POST 'https://zoom.us/oauth/token?grant_type=account_credentials&account_id='"$Zoom_OAuth_ACCOUNT" --header 'Authorization: Basic '$credentials | jq -r '.access_token' )
-#    getHosts
+
+curl -s --location --request POST 'https://zoom.us/oauth/token?grant_type=account_credentials&account_id='"$Zoom_OAuth_ACCOUNT" --header 'Authorization: Basic '"$credentials" > tmp.json
+thisAuth=$(jq -r '.access_token' tmp.json)
 
 curl --request GET \
      --url 'https://api.zoom.us/v2/past_meetings/'"$thisMeeting"'/instances' \
@@ -41,7 +38,9 @@ thisFile=$(jq -r '.meeting_id' thisInstance.json)
 #echo "<img src="WISE-Logo_Owl.jpg">" > $thisFile.md
 thisTitle=$(jq -r '.summary_title' thisInstance.json)
 echo :>$thisFile.md
-jq -r '"\n\n" + .meeting_start_time |  strptime("%Y-%m-%dT%H:%M:%S%Z") | strftime("%B %-e,%Y") ' thisInstance.json >> $thisFile.md
+
+jq -r '"\n\n" + .meeting_start_time | strptime("%Y-%m-%dT%H:%M:%SZ") | strftime("%B %-e, %Y")' thisInstance.json >> $thisFile.md
 jq -r '(.summary_details[]|"\n## " +.label+"\n\n",.summary),"\n\n## Next Steps","1. "+.next_steps[]' thisInstance.json >> $thisFile.md
 pandoc -f gfm --toc -s --metadata title="$thisTitle" -o new.md $thisFile.md
-pandoc --include-in-header=header.md --pdf-engine=weasyprint new.md -o $thisFile.pdf
+SCRIPT_DIR=$(dirname "$0")
+pandoc --include-in-header=$SCRIPT_DIR/header.md --pdf-engine=weasyprint new.md -o $thisFile.pdf --pdf-engine-opt=-q
